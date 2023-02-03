45dfc8ac1d0164876b6ee6a0f43ac01573761b82
khadjaa <islomkhadja2001@icloud.com>
2023-02-03 20:56:18 +0300

    used grid

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,38 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className={"app-wrapper"}>
+            <header className={"header"}>
+                <img src="https://wsd.events/2017/01/28/pres/here-be-dragons/pictures/react-logo.png" alt="logo"/>
+            </header>
+            <nav className={"nav"}>
+                <div><a href={"#"}>Profile</a></div>
+                <div><a href={"#"}>Messages</a></div>
+                <div><a href={"#"}>News</a></div>
+                <div><a href={"#"}>Musics</a></div>
+                <div><a href={"#"}>Settings</a></div>
+            </nav>
+            <main className={"main"}>
+                <div>
+                    <img
+                        src="https://wallup.net/wp-content/uploads/2018/09/30/912666-quotes-typography-text-quote-motivational-inspirational.jpg"
+                        alt="main_img"/>
+                </div>
+                <div>ava + description</div>
+                <div>
+                    My Posts
+                    <div>
+                        New Posts
+                    </div>
+                </div>
+                <div>
+                    <div>Post 1</div>
+                    <div>Post 2</div>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
