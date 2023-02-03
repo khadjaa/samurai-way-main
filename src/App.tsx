6ed520e361a90ed89496ac57bf8bdce7c2e8210e
khadjaa <islomkhadja2001@icloud.com>
2023-02-03 21:17:29 +0300

    Added Profile.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,14 @@ import React from 'react';
 import './App.css';
 import Header from "./components/Header.";
 import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
 
 function App() {
     return (
         <div className={"app-wrapper"}>
             <Header/>
             <Navbar/>
-            <main className={"main"}>
-                <div>
-                    <img
-                        src="https://wallup.net/wp-content/uploads/2018/09/30/912666-quotes-typography-text-quote-motivational-inspirational.jpg"
-                        alt="main_img"/>
-                </div>
-                <div>ava + description</div>
-                <div>
-                    My Posts
-                    <div>
-                        New Posts
-                    </div>
-                </div>
-                <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
-                </div>
-            </main>
+            <Profile/>
         </div>
     );
 }
---
+++ src/components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <main className={"main"}>
+            <div>
+                <img
+                    src="https://wallup.net/wp-content/uploads/2018/09/30/912666-quotes-typography-text-quote-motivational-inspirational.jpg"
+                    alt="main_img"/>
+            </div>
+            <div>ava + description</div>
+            <div>
+                My Posts
+                <div>
+                    New Posts
+                </div>
+            </div>
+            <div>
+                <div>Post 1</div>
+                <div>Post 2</div>
+            </div>
+        </main>
+    )
+}
+
+export default Profile
