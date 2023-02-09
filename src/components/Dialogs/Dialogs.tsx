9ef8f72d946811c046e8b81c71855d8c989f17a9
khadjaa <islomkhadja2001@icloud.com>
2023-02-09 15:28:25 +0300

    added NavLink to users and added props type

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,48 @@
 import s from './Dialogs.module.css';
+import {NavLink} from "react-router-dom";
+
+export type DialogsItemsProps = {
+    id: number
+    name: string
+}
+
+export type MessageItemsProps = {
+    message: string
+}
+
+const MessageItems = (props: MessageItemsProps) => {
+    return (
+        <div>{props.message}</div>
+    )
+}
+
+const DialogsItems = (props: DialogsItemsProps) => {
+    return (
+        <div className={s.dialog + ' ' + s.active}>
+            <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink>
+        </div>
+    )
+}
 
 const Dialogs = () => {
-  return(
-      <div>
-          Dialogs
-      </div>
-  )
+    return (
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <DialogsItems id={1} name={'Islam'}/>
+                <DialogsItems id={2} name={'Senya'}/>
+                <DialogsItems id={3} name={'Genya'}/>
+                <DialogsItems id={4} name={'Artem'}/>
+                <DialogsItems id={5} name={'Sasha'}/>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    <MessageItems message={'Hi'}/>
+                    <MessageItems message={'How is your it?'}/>
+                    <MessageItems message={'Cool'}/>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Dialogs;
