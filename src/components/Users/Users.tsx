1adf7d4002c84cb4caaa2ecf95d413cd8debcf9f
khadjaa <islomkhadja2001@icloud.com>
2023-04-03 13:11:15 +0300

    added life cycle method didMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -6,8 +6,7 @@ import userPhoto from '../../assets/ava.png'
 
 class Users extends React.Component<UsersPropsType> {
 
-    constructor(props: UsersPropsType) {
-        super(props);
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then(response => {
                 this.props.setUsers(response.data.items)
@@ -49,6 +48,6 @@ class Users extends React.Component<UsersPropsType> {
             </div>
         );
     }
-};
+}
 
 export default Users
